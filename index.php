e6410f6aa369774a44ad2eb0b92c608497c05afc
tefcastineiras <54291296+tefcastineiras@users.noreply.github.com>
2023-09-19 18:50:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+echo "Hola mundo!";
+
+echo "<br>" ;
+$variable = "Hola mundo";
+echo $variable;
+
+$numero1 = 10;
+$numero2 = 5;
+echo "<br>" ;
+echo "Suma:";
+echo "<br>" ;
+echo $numero1 + $numero2;
+echo "<br>" ;
+echo "Resta:";
+echo "<br>" ;
+echo $numero1 - $numero2;
+echo "<br>" ;
+echo "Multiplicación:";
+echo "<br>" ;
+echo $numero1 * $numero2; 
+echo "<br>" ;
+echo "División:";
+echo "<br>" ;
+echo $numero1 / $numero2;
+echo "<br>" ;
+echo "Resto:";
+echo "<br>" ;
+echo $numero1 % $numero2;
+echo "<br>" ;
+
+echo " Celsius a Fahrenheit:";
+echo "<br>" ;
+echo(20 * 9/5) + 32 ;
+echo "<br>" ;
+
+$alto = 12;
+$base = 18;
+echo "Perimetro de rectangulo";
+echo "<br>" ;
+echo $alto * 2 + $base * 2; //perimetro
+echo "<br>" ;
+echo "Área de rectangulo";
+echo "<br>" ;
+echo $alto * $base; //area
+
+echo "<br>" ;
+
+$radio = 30 ;
+$pi = 3.14 ;
+echo "Perimetro de círculo:";
+echo "<br>" ;
+echo 2 * $pi * $radio;//perimetro
+echo "<br>" ;
+echo "Área de círculo:";
+echo "<br>" ;
+echo $pi * ($radio * $radio);//area
+
+?>
